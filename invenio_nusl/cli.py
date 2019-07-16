e66101a2250ac94f0ad4ebfd03a428928f1668c7
semtex <Daniel.Kopecky@techlib.cz>
2019-07-16 12:00:02 +0200

    Implementing taxonomies

--- invenio_nusl/cli.py
+++ invenio_nusl/cli.py
@@ -1,10 +1,12 @@
 import json
 
 import click
-from flask.cli import with_appcontext
+from flask import cli
 
 from flask_taxonomies.managers import TaxonomyManager
 from flask_taxonomies.models import Taxonomy, TaxonomyTerm
+from invenio_db import db
+# from invenio_db import InvenioDB
 
 
 @click.group()
@@ -12,18 +14,38 @@ def nusl():
     """Nusl commands."""
 
 
-@with_appcontext
-@nusl.command('import_taxonomies')
-@click.option("-p", "--path", type=click.STRING)
-def import_taxonomies(path):
-    with open(path, "r") as f:
-        taxonomy_dict = json.load(f)
+@cli.with_appcontext
+@nusl.command('import_studyfields')
+def import_studyfields():
+    studyfields = Taxonomy(code='studyfields', extra_data={
+        "title": [
+            {
+                "name": "studijní obory",
+                "lang": "cze"
+            },
+            {
+                "name": "study fields",
+                "lang": "eng"
+            }
+        ]
+    }
+                           )
 
-    for k in taxonomy_dict.keys():
-        print(k)
-    # t = Taxonomy(code='vskp')
-    # m = TaxonomyManager()
-    # term = m.create('bakalarske_prace', title={'cs': 'Bakalářské práce'}, path='/vskp/', extra_data={})
-
-    # db.session.add()
+    db.session.add(studyfields)
+    db.session.commit()
+    # manager = TaxonomyManager()
+    # term = manager.create('6208R082',
+    #                       title={
+    #                           "name": "Podnikatelská činnost",
+    #                       },
+    #                       path='/studyfields/',
+    #                       extra_data={"code": "6208R082"})
+    # print(studyfields, term)
+    #
+    # db.session.add(studyfields)
+    # db.session.add(term)
     # db.session.commit()
+
+
+if __name__ == "__main__":
+    import_studyfields()
