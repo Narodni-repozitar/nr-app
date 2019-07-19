67ecfc954ee931e2639dc53541e8e4dc44273737
semtex <Daniel.Kopecky@techlib.cz>
2019-07-19 15:53:46 +0200

    Implementing taxonomies

--- invenio_nusl/cli.py
+++ invenio_nusl/cli.py
@@ -249,5 +249,189 @@ def import_doctype():
                 print(f"{counter}. {term}")
 
 
+@nusl.command('import_riv')
+@cli.with_appcontext
+def import_riv():
+    manager = TaxonomyManager()
+    if manager.get_taxonomy('doctype') is None:
+        doctype = Taxonomy(code='doctype', extra_data={
+            "title": [
+                {
+                    "name": "Typ dokumentu",
+                    "lang": "cze"
+                },
+                {
+                    "name": "Type of document",
+                    "lang": "eng"
+                }
+            ]
+        })
+        db.session.add(doctype)
+        db.session.commit()
+
+    path = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(path, "data", "document_typology_RIV.csv")
+
+    with open(path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=",")
+        counter = 0
+        for row in reader:
+            counter += 1
+            print(f"{counter}.", "#######################################################")
+            try:
+                manager.get_from_path(f'/doctype/RIV')
+            except AttributeError:
+                term = manager.create("RIV",
+                                      title={
+                                          "name": "RIV",
+                                      },
+                                      path=f'/doctype/',
+                                      )
+                db.session.add(term)
+                db.session.commit()
+                print(f"/doctype/RIV")
+
+            try:
+                manager.get_from_path(f'/doctype/RIV/{row["base_code"]}')
+            except AttributeError:
+                if row["sub_code"] == "":
+                    term = manager.create(row["base_code"].strip(),
+                                          title={
+                                              "name": row["name"]
+                                          },
+                                          extra_data={
+                                              "definition": row["definice"]
+                                          },
+                                          path=f'/doctype/RIV',
+                                          )
+                else:
+                    term = manager.create(row["base_code"].strip(),
+                                          title={
+                                              "name": row["base_code"]
+                                          },
+                                          path=f'/doctype/RIV',
+                                          )
+                db.session.add(term)
+                db.session.commit()
+
+                print(f"/doctype/RIV/{row['base_code']}")
+
+            if row["sub_code"] != "":
+                try:
+                    manager.get_from_path(f"/doctype/RIV/{row['base_code']}/{row['sub_code']}")
+                except AttributeError:
+                    term = manager.create(row["sub_code"].strip(),
+                                          title={
+                                              "name": row["name"],
+                                          },
+                                          extra_data={
+                                              "definition": row["definice"]
+                                          },
+                                          path=f"/doctype/RIV/{row['base_code']}"
+                                          )
+
+                    print(f"/doctype/RIV/{row['base_code']}/{row['sub_code']}")
+
+                    db.session.add(term)
+                    db.session.commit()
+
+
+@nusl.command('import_providers')
+@cli.with_appcontext
+def import_providers():
+    def _split_export(export: str):
+        export = export.replace("---", "|||")
+        export_array = export.split("|||")
+        export_dict = {
+            "id": export_array[0],
+            "name": export_array[1],
+            "address": export_array[2],
+            "url": export_array[3],
+            "lib_url": export_array[4]
+        }
+        return export_dict
+
+    manager = TaxonomyManager()
+    if manager.get_taxonomy('provider') is None:
+        provider = Taxonomy(code='provider', extra_data={
+            "title": [
+                {
+                    "name": "Poskytovatel záznamu",
+                    "lang": "cze"
+                },
+                {
+                    "name": "Provider of record",
+                    "lang": "eng"
+                }
+            ]
+        })
+        db.session.add(provider)
+        db.session.commit()
+
+    path = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(path, "data", "Institutions_ids.csv")
+
+    with open(path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=",")
+        counter = 0
+        for row in reader:
+            counter += 1
+            print(counter, "#######################################################")
+            try:
+                manager.get_from_path(f'/provider/{row["isPartOf2"]}')
+            except AttributeError:
+                term = manager.create(row["isPartOf2"].strip(),
+                                      title={
+                                          "name": row["name_isPartOf2"],
+                                      },
+                                      path=f'/provider/',
+                                      )
+                db.session.add(term)
+                db.session.commit()
+                print(f"/provider/{row['isPartOf2']}")
+
+            try:
+                manager.get_from_path(f'/provider/{row["isPartOf2"]}/{row["isPartOf"]}')
+            except AttributeError:
+                if row["isPartOf"] != "null":
+                    term = manager.create(row["isPartOf"].strip(),
+                                          title={
+                                              "name": row["name_isPartOf"],
+                                          },
+                                          path=f'/provider/{row["isPartOf2"]}/',
+                                          )
+                    db.session.add(term)
+                    db.session.commit()
+
+                    print(f"/provider/{row['isPartOf2']}/{row['isPartOf']}")
+
+            try:
+                manager.get_from_path(f'/provider/{row["isPartOf2"]}/{row["isPartOf"]}/{row["id"]}')
+            except AttributeError:
+                if row["isPartOf"] != "null":
+                    term = manager.create(row["id"].strip(),
+                                          title={
+                                              "name": _split_export(row["EXPORT"])["name"],
+                                          },
+                                          path=f'/provider/{row["isPartOf2"]}/{row["isPartOf"]}',
+                                          extra_data=_split_export(row["EXPORT"])
+                                          )
+
+                    print(f"/provider/{row['isPartOf2']}/{row['isPartOf']}/{row['id']}")
+                else:
+                    term = manager.create(row["id"].strip(),
+                                          title={
+                                              "name": _split_export(row["EXPORT"])["name"],
+                                          },
+                                          path=f'/provider/{row["isPartOf2"]}/',
+                                          extra_data=_split_export(row["EXPORT"])
+                                          )
+
+                    print(f"/provider/{row['isPartOf2']}/{row['id']}")
+
+                db.session.add(term)
+                db.session.commit()
+
+
 if __name__ == "__main__":
     pass
