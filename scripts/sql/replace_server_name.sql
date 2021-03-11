8fc3826f6edd08a6d7382ee931512445a981ce9c
Daniel Kopecký <kopecky.d@gmail.com>
2021-03-11 14:19:52 +0100

    Script for replacing server name

--- scripts/sql/replace_server_name.sql
+++ scripts/sql/replace_server_name.sql
@@ -1,2 +1,3 @@
-SELECT Replace(json::text, 'http://127.0.0.1:5000', 'http://127.0.0.1:8080')::jsonb
-FROM records_metadata;
+UPDATE records_metadata
+SET    json = Replace(json::text, 'http://127.0.0.1:5000', 'https://repozitar.narodni-repozitar.cz')::jsonb;
+-- https://stackoverflow.com/questions/51005106/postgresql-find-and-replace-in-jsonb-data-as-text
