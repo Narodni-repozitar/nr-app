d5ec00c6508db130ad5300af1dc26cfe6d37b086
Mirek Simek <miroslav.simek@gmail.com>
2021-04-07 11:14:21 +0200

    parallel version

--- scripts/set_initial_communities.py
+++ scripts/set_initial_communities.py
@@ -1,42 +1,51 @@
-import json
+from multiprocessing.pool import ThreadPool
 
+import itertools
 from invenio_app.factory import create_api
 from invenio_db import db
 from invenio_records.models import RecordMetadata
 from sqlalchemy.orm.attributes import flag_modified
-from tqdm import tqdm
 
 app = create_api()
 
 
+def set_providers(*mds):
+    with app.app_context():
+        with db.session.begin_nested():
+            for md in RecordMetadata.query.filter(RecordMetadata.id.in_(mds)):
+                providers = md.json.get('provider')
+
+                primary_community = None
+
+                if providers:
+                    provider = None
+                    for p in providers:
+                        if not provider or p['level'] > provider['level']:
+                            provider = p
+                    provider = providers[0]
+                    self_link = provider.get('links', {}).get('self')
+                    if self_link:
+                        primary_community = self_link.strip('/').rsplit('/', maxsplit=1)[-1]
+
+                md.json['_administration'] = {
+                    "state": 'new',
+                    "primaryCommunity": primary_community,
+                    "communities": []
+                }
+                flag_modified(md, 'json')
+                db.session.add(md)
+        db.session.commit()
+
+
+def grouper(n, iterable):
+    iterable = iter(iterable)
+    return iter(lambda: list(itertools.islice(iterable, n)), [])
+
 def run():
-    print('Running que ry')
-    for md in tqdm(
-            db.session.query(RecordMetadata).yield_per(10000),
-            total=db.session.query(RecordMetadata).count()
-    ):
-        providers = md.json.get('provider')
-
-        primary_community = None
-
-        if providers:
-            provider = None
-            for p in providers:
-                if not provider or p['level'] > provider['level']:
-                    provider = p
-            provider = providers[0]
-            self_link = provider.get('links', {}).get('self')
-            if self_link:
-                primary_community = self_link.strip('/').rsplit('/', maxsplit=1)[-1]
-
-        md.json['_administration'] = {
-            "state": 'new',
-            "primaryCommunity": primary_community,
-            "communities": []
-        }
-        flag_modified(md, 'json')
-        db.session.add(md)
-    db.session.commit()
+    md_stream = [x[0] for x in db.session.query(RecordMetadata.id).distinct()]
+    with ThreadPool(processes=5) as pool:
+        pool.starmap(set_providers, grouper(10000, md_stream))
+    pool.join()
 
 
 if __name__ == '__main__':
