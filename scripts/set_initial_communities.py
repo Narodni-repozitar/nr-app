1bcd4b8810ab0e44a9f6ad3b43f6ec07bc4e39b7
Daniel Kopecký <kopecky.d@gmail.com>
2021-04-08 15:18:22 +0200

    Print control_number

--- scripts/set_initial_communities.py
+++ scripts/set_initial_communities.py
@@ -1,10 +1,8 @@
-import threading
+import itertools
+import time
+from multiprocessing.pool import Pool
 from random import random
 
-import time
-from multiprocessing.pool import ThreadPool, Pool
-
-import itertools
 from invenio_app.factory import create_api
 from invenio_db import db
 from invenio_records.models import RecordMetadata
@@ -18,6 +16,8 @@ def set_providers(*mds):
     with app.app_context():
         with db.session.begin_nested():
             for md in RecordMetadata.query.filter(RecordMetadata.id.in_(mds)):
+                control_number = md.json.get('control_number')
+                print(f"Control number: {control_number}")
                 providers = md.json.get('provider')
 
                 primary_community = None
@@ -46,13 +46,15 @@ def grouper(n, iterable):
     iterable = iter(iterable)
     return iter(lambda: list(itertools.islice(iterable, n)), [])
 
+
 def initiate():
     time.sleep(10 * random())
+
 
 def run():
     md_stream = [x[0] for x in db.session.query(RecordMetadata.id).distinct()]
     md_stream = list(grouper(1000, md_stream))
-    with Pool(processes=5, initializer=initiate) as pool:
+    with Pool(processes=1, initializer=initiate) as pool:
         pool.starmap(set_providers, tqdm(md_stream))
     pool.join()
 
